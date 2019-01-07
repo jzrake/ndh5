772c39889245bd60e74a5bb1590746667bbaf720
Jonathan Zrake <jonathan.zrake@gmail.com>
2019-01-06 20:19:27 -0500

    Add juce module

---
+++ juce_ndh5/juce_ndh5.h
@@ -0,0 +1,19 @@
+/******************************************************************************
+BEGIN_JUCE_MODULE_DECLARATION
+
+  ID:               juce_ndh5
+  vendor:           Jonathan Zrake
+  version:          0.0.1
+  name:             ndh5
+  description:      Header-only HDF5 wrapper for modern C++ modeled on h5py
+  website:          https://github.com/jzrake/ndh5
+  license:          GPL
+  dependencies:     juce_ndarray
+
+END_JUCE_MODULE_DECLARATION
+******************************************************************************/
+
+
+#pragma once
+#define JUCE_NDH5_INCLUDED
+#include <juce_ndh5/ndh5.hpp>
